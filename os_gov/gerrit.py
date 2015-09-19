e1f238def7eb5e7dbbc337ad8d05627c2caa954a
Kun Huang <gareth@openstacker.org>
2015-09-19 15:35:39 +0800

    add basic gerrit module

---
+++ os_gov/gerrit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: Kun Huang <academicgareth@gmail.com>
+
+from gerritlab import gerrit
+
+class Gerrit(gerrit.Gerrit):
+    def __init__(self, username):
+        self.host = "review.openstack.org"
+        self.user = username
+        super(Gerrit, self).__init__(hostname=self.host, username=self.user)
