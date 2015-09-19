28d534e8309819dba36135a30e964d876acd4df2
Kun Huang <gareth@openstacker.org>
2015-09-19 17:41:28 +0800

    add scripts to get all core members

---
+++ tools/all_cores.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: Kun Huang <academicgareth@gmail.com>
+
+
+from os_gov import gerrit
+from copy import deepcopy
+
+g = gerrit.Gerrit()
+all_groups = g.listGroups()
+print "all groups: %s" % len(all_groups)
+all_members = {}
+iter_ = 0
+for group in all_groups:
+    if " " in group:
+        continue
+    members = g.listMembers(group)
+    print "[%s] count %s in group %s" % (iter_, len(members), group)
+    iter_ += 1
+    #if iter_ >= 10: break
+    for member in members:
+        if member in all_members:
+            all_members[member]["count"] += 1
+            all_members[member]["resp"].append(group)
+        else:
+            all_members[member] = deepcopy(members[member])
+            all_members[member]["count"] = 1
+            all_members[member]["resp"] = [group]
+
+print all_members
